d57f609f8549d79cdd5724f77e6ca92b42300690
Sohelrana2815 <sohel152302@gmail.com>
2025-01-15 10:12:32 +0600

    make portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
+import Portfolio from "./Components/Portfolio/Portfolio";
 import SocialLinks from "./Components/SocialLinks/SocialLinks";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Portfolio />
       <SocialLinks />
     </>
   );
