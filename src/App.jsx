39ec0f28a5e10e46d3286b24e095d213e4f4dabf
Sohelrana2815 <sohel152302@gmail.com>
2025-01-15 10:10:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import SocialLinks from "./Components/SocialLinks/SocialLinks";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <SocialLinks />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,36 @@
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+          tempore at. Ipsam perferendis repellat, doloremque soluta porro sequi.
+          Minima, aliquam. Itaque aliquam autem sit in illo hic aperiam optio et
+          aspernatur iste est nobis eligendi dolore omnis cum, modi incidunt!
+          Est non expedita unde natus assumenda, facere tempora necessitatibus
+          minus.
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt omnis
+          assumenda, inventore explicabo nihil tempore et doloribus! Animi quasi
+          corrupti quia quam, esse beatae aperiam maxime assumenda cumque sequi
+          laborum modi ipsam similique eaque sunt distinctio dolorem. Optio quo
+          laborum earum neque eligendi a natus nihil minima deleniti! Rem,
+          commodi.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
